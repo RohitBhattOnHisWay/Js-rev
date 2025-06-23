2d9618a2dc2040efe90a6e7aad0458eec262135a
RohitBhattOnHisWay <rohitbhatt.103562@gmail.com>
2025-06-23 06:19:25 +0000

    conversion of datatypes

---
+++ 01_basicsJS/01_var.js
@@ -0,0 +1,11 @@
+let hello="ROhit";
+const jj=23;
+var idProof="Adhar Card";
+accountNo=22345;
+//jj=34;This is not allowed 
+//we dont use var due to scope issue 
+/*
+Prefer not to use var due to issue in block scope and function
+*/
+let AccountState;
+console.table([hello,jj,idProof,accountNo,AccountState]);
---
+++ 01_basicsJS/03_conversionop.js
@@ -0,0 +1,13 @@
+let score=true
+console.log(typeof score);
+console.log(typeof(score));
+ let valueInit=Number(score);
+ console.log(typeof(valueInit));
+ console.log(valueInit);
+ //"33 =>33
+ //"33abc"=>NaN
+ //true=1
+ let isloggedin=1
+ let booleancha=Boolean(isloggedin);
+ console.log(booleancha);
+ 
