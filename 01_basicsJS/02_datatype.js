7ab93b4328bdb967a60eecdea98f3840b4902760
RohitBhattOnHisWay <rohitbhatt.103562@gmail.com>
2025-06-23 06:01:35 +0000

    container confi github

---
+++ 01_basicsJS/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";//Treat  all js code as newer version
+//alert (2+3) not suport in node 
+
+
+let name="ROhit"
+let age=23
+let char='A'
+let login=false
+let state=null
+console.log(typeof(age));
+/*
+number =>2 to power 53
+bigint
+string
+null=>standalone value
+undefined=>
+symbol=>unique
+.....
+object(imp)
+*/
